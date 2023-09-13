c754b805900954d93da6c4d020dd46411ef32717
xiangjuncheng3 <xiangjuncheng@jd.com>
2023-09-13 14:16:03 +0800

    add path config cmd

--- apk.js
+++ apk.js
@@ -1,210 +1,263 @@
 const fs = require("fs");
-const path = require('path')
+const path = require("path");
 const child_process = require("child_process");
-const https = require('https');
-const QRCode = require('qrcode-reader');
+const https = require("https");
+const QRCode = require("qrcode-reader");
 
 let apkInfos = [];
 
-
 exports.apkEnter = async (action) => {
-    // window.removeDBItem('adbPath')
-    // window.removeDBItem('downloadPath')
-    const downloadRes = await downloadConfig();
-    if (!downloadRes) {
-        return;
+  const downloadRes = await downloadConfig();
+  if (!downloadRes) {
+    return;
+  }
+  const adbRes = await adbConfig();
+  console.info(adbRes);
+  if (!adbRes) {
+    return;
+  }
+  if ((action.type = "files")) {
+    console.log(typeof action.payload);
+    if (typeof action.payload == "string") {
+      if (action.payload.startsWith("data:image")) {
+        // str 是字符串
+        window.showMsg("解析二维码");
+        scanImage(action.payload);
+      } else {
+        window.showMsg("下载并安装apk链接");
+        apkDownloadAndInstall(action.payload);
+      }
+    } else {
+      window.showMsg(`安装APK ${action.payload[0]}`);
+      apkInstall(action.payload[0].path);
     }
-    const adbRes = await adbConfig()
-    if (!adbRes) {
-        return;
-    }
-    if (action.type = 'files') {
-        console.log(typeof (action.payload))
-        if (typeof (action.payload) == 'string') {
-            if (action.payload.startsWith('data:image')) {
-                // str 是字符串 
-                window.showMsg('解析二维码')
-                scanImage(action.payload)
-            } else {
-                window.showMsg('下载并安装apk链接')
-                apkDownloadAndInstall(action.payload)
-            }
-        } else {
-            window.showMsg(`安装APK ${action.payload[0]}`)
-            apkInstall(action.payload[0].path)
-        }
-    } else if (action.type = 'img') {
-        window.showMsg('解析二维码')
-        scanImage(action.payload)
-    } else if (action.type = 'regex') {
-        window.showMsg('下载并安装apk链接')
-        apkDownloadAndInstall(action.payload)
-    }
-}
+  } else if ((action.type = "img")) {
+    window.showMsg("解析二维码");
+    scanImage(action.payload);
+  } else if ((action.type = "regex")) {
+    window.showMsg("下载并安装apk链接");
+    apkDownloadAndInstall(action.payload);
+  }
+};
+
+exports.apkPathEnter = async (action) => {
+  switch (action.payload) {
+    case "APK环境清除":
+      window.removeDBItem("downloadPath");
+      window.removeDBItem("adbPath");
+      window.showMsg("APK环境清除成功", false, true);
+      break;
+    case "ADB环境配置":
+      window.removeDBItem("adbPath");
+      const adbRes = await adbConfig(true);
+      console.info(adbRes);
+      if (adbRes) {
+        window.showMsg("ADB环境配置成功", false, true);
+      }
+      break;
+    case "APK下载路径":
+      window.removeDBItem("downloadPath");
+      const downloadRes = await downloadConfig();
+      if (downloadRes) {
+        window.showMsg("APK下载路径配置成功", false, true);
+      }
+      break;
+
+    default:
+      break;
+  }
+};
 
 exports.getApkInfos = () => {
-    return downloads
-}
-
+  return downloads;
+};
 
 function apkDownloadAndInstall(url) {
-    let apkUrl;
-    if (url == 'apkt') {
-        apkUrl = "https://storage.jd.com/com.bamboo.android.product/490/13763356/JDMALLLITE-6.11.5-22710-tjDebugUseReleaseSign_64bit_resguard-202309081159_sec_signed.apk";
-    } else {
-        apkUrl = url;
-    }
-    downloadApk(apkUrl, window.getDBItem('downloadPath'))
-        .then((apkInfo) => {
-            // saveApkInfo(url, fileName, apkPath);
-            const [url, fileName, apkPath] = apkInfo;
-            window.showMsg(`下载完成，安装中 ${apkPath}`);
-            apkInstall(apkPath);
-        })
-        .catch(err => {
-            window.showMsg('下载失败:' + err);
-        });
+  let apkUrl;
+  if (url == "apkt") {
+    apkUrl =
+      "https://storage.jd.com/com.bamboo.android.product/490/13763356/JDMALLLITE-6.11.5-22710-tjDebugUseReleaseSign_64bit_resguard-202309081159_sec_signed.apk";
+  } else {
+    apkUrl = url;
+  }
+  downloadApk(apkUrl, window.getDBItem("downloadPath"))
+    .then((apkInfo) => {
+      // saveApkInfo(url, fileName, apkPath);
+      const [url, fileName, apkPath] = apkInfo;
+      window.showMsg(`下载完成，安装中 ${apkPath}`);
+      apkInstall(apkPath);
+    })
+    .catch((err) => {
+      window.showMsg("下载失败:" + err);
+    });
 }
 
 let apkInstall = (apkPath) => {
-    child_process.exec(
-        getCmd(
-            apkPath,
-            window.getDBItem('adbPath')
-        ),
-        (error, stdout, stderr) => {
-            if (error) {
-                var lines = error.message.trim().split("\n");
-                var lastLine = lines[lines.length - 1];
-                window.showMsg(`安装失败:${lastLine}`, false, true);
-                return;
-            }
-            window.showMsg(`安装完成:${stdout}`, false, true);
-        }
-    );
-}
+  child_process.exec(
+    getCmd(apkPath, window.getDBItem("adbPath")),
+    (error, stdout, stderr) => {
+      if (error) {
+        var lines = error.message.trim().split("\n");
+        var lastLine = lines[lines.length - 1];
+        window.showMsg(`安装失败:${lastLine}`, false, true);
+        return;
+      }
+      window.showMsg(`安装完成:${stdout}`, false, true);
+    }
+  );
+};
 
 async function downloadConfig() {
-    if (window.getDBItem('downloadPath') == null) {
-        const path = await window.showOpenDialog({
-            properties: ['openDirectory'],
-            title: '选择下载位置',
-            defaultPath: window.getUtoolPath('downloads')
-        })
-        console.warn(path)
+  if (window.getDBItem("downloadPath") == null) {
+    window.showMsg("请设置apk下载路径", false, true);
+    const path = await window.showOpenDialog({
+      properties: ["openDirectory"],
+      title: "选择下载位置",
+      defaultPath: window.getUtoolPath("downloads"),
+    });
+    console.warn(path);
 
-        window.setDBItem('downloadPath', path ? path[0] : null)
-        if (path == null) {
-            window.showMsg("下载路径未配置", false, true)
-            return false;
-        } else {
-            return true;
-        }
+    window.setDBItem("downloadPath", path ? path[0] : null);
+    if (path == null) {
+      window.showMsg("下载路径未配置", false, true);
+      return false;
+    } else {
+      return true;
     }
-    return true;
+  }
+  return true;
 }
 
-async function adbConfig() {
-    if (window.getDBItem('adbPath') == null) {
-        const { stdout, stderr } = await child_process.exec('adb');
-        let path = null
-        if (stdout) {
-            path = ['adb']
-        } else {
-            path = await window.showOpenDialog({
-                properties: ['openFile'],
-                title: '选择adb位置'
-            })
-        }
-
-        window.setDBItem('adbPath', path ? path[0] : null)
-        if (path == null) {
-            window.showMsg("adb环境未配置", false, true)
-            return false;
-        } else {
-            return true;
-        }
-
+async function adbConfig(ignoreDefault = false) {
+  if (window.getDBItem("adbPath") == null) {
+    let path = null;
+    try {
+      if (ignoreDefault) {
+        throw new Error();
+      }
+      const { stdout, stderr } = await runCommand("adb");
+      // 处理成功的情况
+      console.log(`存在adb参数:${stdout}`);
+      path = ["adb"];
+    } catch (error) {
+      // 处理错误的情况
+      console.log(`不存在adb参数:${error}`);
+      window.showMsg("请设置adb环境路径", false, true);
+      path = await window.showOpenDialog({
+        properties: ["openFile"],
+        title: "选择adb位置",
+      });
     }
+    window.setDBItem("adbPath", path ? path[0] : null);
+    if (path == null) {
+      window.showMsg("adb环境未配置", false, true);
+      return false;
+    } else {
+      return true;
+    }
+
+    // const { stdout, stderr } = child_process.exec("adb");
+    // let path = null;
+    // console.log(`$stdout:${stdout}`);
+    // console.log(`stderr:${stderr}`);
+    // if (stdout) {
+    //   path = ["adb"];
+    // } else {
+    //   path = await window.showOpenDialog({
+    //     properties: ["openFile"],
+    //     title: "选择adb位置",
+    //   });
+    // }
+  } else {
     return true;
+  }
 }
 
 const downloadApk = async (url, savePath) => {
-    // const fileName = url.split('/').pop();
-    const fileName = 'downTemp.apk';
-    savePath = path.join(savePath, fileName);
-    const file = window.createWriteStream(savePath);
+  // const fileName = url.split('/').pop();
+  const fileName = "downTemp.apk";
+  savePath = path.join(savePath, fileName);
+  const file = window.createWriteStream(savePath);
 
-    const response = await new Promise((resolve, reject) => {
-        https.get(url, res => {
-            resolve(res);
+  const response = await new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      resolve(res);
 
-            res.on('error', err => {
-                reject(err);
-            });
-        });
+      res.on("error", (err) => {
+        reject(err);
+      });
     });
+  });
 
-    response.pipe(file);
+  response.pipe(file);
 
-    return new Promise((resolve, reject) => {
-
-        let totalBytes = 0;
-        response.on('data', chunk => {
-            totalBytes += chunk.length;
-            const progress = (totalBytes / response.headers['content-length']) * 100;
-            window.showMsg(`下载进度: ${progress.toFixed(2)}%`, true);
-        });
-        file.on('finish', () => {
-            console.log(savePath)
-            resolve([url, fileName, savePath])
-        });
-        file.on('error', err => reject(err));
-
+  return new Promise((resolve, reject) => {
+    let totalBytes = 0;
+    response.on("data", (chunk) => {
+      totalBytes += chunk.length;
+      const progress = (totalBytes / response.headers["content-length"]) * 100;
+      window.showMsg(`下载进度: ${progress.toFixed(2)}%`, true);
     });
-
+    file.on("finish", () => {
+      console.log(savePath);
+      resolve([url, fileName, savePath]);
+    });
+    file.on("error", (err) => reject(err));
+  });
 };
 
 let getCmd = (filePath, adb) => {
-    command = `${adb} install -r ${filePath}`;
-    console.log(command);
-    return command;
+  command = `${adb} install -r ${filePath}`;
+  console.log(command);
+  return command;
 };
 
 let saveApkInfo = (url, fileName, apkPath) => {
-    let downloads = getApkInfos();
-    downloads.push({
-        name: fileName,
-        path: filePath,
-        time: new Date()
+  let downloads = getApkInfos();
+  downloads.push({
+    name: fileName,
+    path: filePath,
+    time: new Date(),
+  });
+  if (downloads.length > 5) {
+    let fileInfo = downloads.shift();
+    fs.rm(fileInfo.path, { recursive: true }, (err) => {
+      if (err) throw err;
+      console.log(`${fileInfo.path} Directory deleted!`);
     });
-    if (downloads.length > 5) {
-        let fileInfo = downloads.shift();
-        fs.rm(fileInfo.path, { recursive: true }, (err) => {
-            if (err) throw err;
-            console.log(`${fileInfo.path} Directory deleted!`);
-        });
-    }
-}
+  }
+};
 
 let scanImage = (imageBase64) => {
-    const qr = new QRCode();
-    qr.callback = (err, value) => {
-        if (err) {
-            window.showMsg('无法识别图片中二维码', false, true)
-            return
-        }
-
-        let result = value.result
-        console.log(result)
-        const isApkUrl = /^http.*\.apk$/.test(result)
-        if (isApkUrl) {
-            apkDownloadAndInstall(result)
-        } else {
-            window.showMsg('图片中二维码非apk url', false, true)
-        }
+  const qr = new QRCode();
+  qr.callback = (err, value) => {
+    if (err) {
+      window.showMsg("无法识别图片中二维码", false, true);
+      return;
     }
 
-    qr.decode(imageBase64);
-}
+    let result = value.result;
+    console.log(result);
+    const isApkUrl = /^http.*\.apk$/.test(result);
+    if (isApkUrl) {
+      apkDownloadAndInstall(result);
+    } else {
+      window.showMsg("图片中二维码非apk url", false, true);
+    }
+  };
 
+  qr.decode(imageBase64);
+};
+
+function runCommand(cmd) {
+  return new Promise((resolve, reject) => {
+    child_process.exec(cmd, (error, stdout, stderr) => {
+      console.info(error, stdout, stderr);
+      if (error) {
+        reject(error);
+      } else {
+        resolve({ stdout, stderr });
+      }
+    });
+  });
+}
--- preload.js
+++ preload.js
@@ -1,20 +1,24 @@
 const fs = require("fs");
-const path = require('path')
+const path = require("path");
 const child_process = require("child_process");
-const https = require('https');
-const { apkEnter } = require("./apk");
+const https = require("https");
+const { apkEnter, apkPathEnter } = require("./apk");
 
 window.exports = {
-  "apkInstall": {
+  apkInstall: {
     // 注意：键对应的是 plugin.json 中的 features.code
     mode: "none", // 用于无需 UI 显示，执行一些简单的代码
     args: {
       enter: async (action) => {
-        console.log(action)
+        console.log(action);
         try {
-          apkEnter(action)
+          if (action.code == "apkInstall") {
+            apkEnter(action);
+          } else if (action.code == "apkPath") {
+            apkPathEnter(action);
+          }
         } catch (e) {
-          console.error(e)
+          console.error(e);
         }
         // console.log('运行成功')
         // alert('运行成功')
@@ -29,48 +33,64 @@ window.exports = {
       },
     },
   },
+  apkPath: {
+    // 注意：键对应的是 plugin.json 中的 features.code
+    mode: "none", // 用于无需 UI 显示，执行一些简单的代码
+    args: {
+      enter: async (action) => {
+        console.log(action);
+        try {
+            apkPathEnter(action);
+        } catch (e) {
+          console.error(e);
+        }
+        // console.log('运行成功')
+        // alert('运行成功')
+      }
+    },
+  },
 };
 
 window.showMsg = (msg, noLog, needNotify) => {
   if (!noLog) {
-    console.log(msg)
+    console.log(msg);
   }
   utools.setSubInput(({ text }) => {
-    console.log(text)
+    console.log(text);
   }, msg);
   if (needNotify) {
-    showNotification(msg)
+    showNotification(msg);
   }
-}
+};
 
 let showNotification = (content) => {
-  utools.showNotification(content)
-}
+  utools.showNotification(content);
+};
 
 window.getDBItem = (key) => {
-  return utools.dbStorage.getItem(key)
-}
+  return utools.dbStorage.getItem(key);
+};
 
 window.setDBItem = (key, value) => {
-  utools.dbStorage.setItem(key, value)
-}
+  utools.dbStorage.setItem(key, value);
+};
 
 window.removeDBItem = (key) => {
-  utools.dbStorage.removeItem(key)
-}
+  utools.dbStorage.removeItem(key);
+};
 
 window.hideMainWindow = () => {
-  window.utools.hideMainWindow()
-}
+  window.utools.hideMainWindow();
+};
 
 window.showOpenDialog = async (obj) => {
-  return await utools.showOpenDialog(obj)
-}
+  return await utools.showOpenDialog(obj);
+};
 
 window.getUtoolPath = (path) => {
-  return utools.getPath(path)
-}
+  return utools.getPath(path);
+};
 
 window.createWriteStream = (savePath) => {
-  return fs.createWriteStream(savePath)
-}
+  return fs.createWriteStream(savePath);
+};
